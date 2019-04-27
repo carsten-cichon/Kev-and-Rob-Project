928211232fe4272a6d504cf5a3f885fbbd73111f
kpmcdowellmo <kpmcdowellmo@gmail.com>
2019-04-26 20:12:43 -0500

    pushing code from last work time

--- keylogger/keylogger.cpp
+++ keylogger/keylogger.cpp
@@ -1,9 +1,18 @@
-#include "HttpRequest.h"
-#include "ServerRequest.h"
-#include <windows.h>
+// #include "HttpRequest.h"
+// #include "ServerRequest.h"
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
 
-int main(int argc, char** argv){
-
-
+int main(int argc, char **argv)
+{
+    while (true)
+    {
+        if (kbhit() != 0)
+        {
+            std::cout << getch() << std::endl;
+            std::cout << kbhit() << std::endl;
+        }
+    }
     return 0;
 }
